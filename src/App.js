ccf5ff8e9a68f9c261f0a3e845087a7ceeff3dc9
Nitin-Dwivedi <nitindwivedi964@gmail.com>
2021-09-08 21:37:28 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -25,7 +25,17 @@ function App() {
     }, 1500);
   }
 
-  const toggleMode = () => {
+  const removeBodyClasses = ()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success') 
+  }
+
+  const toggleMode = (cls) => {
+    removeBodyClasses();
+    document.body.classList.add('bg-' + cls)
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
@@ -44,6 +54,7 @@ function App() {
         <Alert alert={alert} />
         <div className="container my-3">
           <Switch>
+            
             {/* /users --> Component 1
         /users/home --> Component 2 */}
             <Route exact path="/about">
--- src/Components/About.js
+++ src/Components/About.js
@@ -37,13 +37,15 @@ export default function About(props) {
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingThree">
-                        <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse"
+                         data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                             <strong>Browser Compatible </strong>
                         </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                         <div className="accordion-body" style={myStyle}>
-                            This word counter software works in any web browsers such as Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to count characters in facebook, blog, books, excel document, pdf document, essays, etc.
+                            This word counter software works in any web browsers such as Chrome, Firefox, Internet Explorer,
+                             Safari, Opera. It suits to count characters in facebook, blog, books, excel document, pdf document, essays, etc.
 
                         </div>
                     </div>
